5458f90d489a61c85fadaf7fd586f26da2b537a9
Elizabeth Eidelson <fluturecode@gmail.com>
2022-03-28 22:07:48 -0400

    added Failure unit tests to Land.test.js

--- test/Land.test.js
+++ test/Land.test.js
@@ -2,6 +2,8 @@ const Land = artifacts.require('./Land');
 
 require('chai').use(require('chai-as-promised')).should();
 
+const EVM_REVERT = 'VM Exception while processing transaction: revert'
+
 contract('Land', ([owner1, owner2]) => {
   const NAME = 'Fluture Buildings';
   const SYMBOOL = 'DUB';
@@ -55,6 +57,20 @@ contract('Land', ([owner1, owner2]) => {
       })
     })
 
-  
+    describe('Failure', () => {
+      it('Prevents mint with 0 value', async () => {
+        await land.mint(1, {from: owner1, value: 0}).should.be.rejectedWith(EVM_REVERT)
+      })
+
+      it('Prevents mint with invalid ID', async () => {
+        await land.mint(100, {from: owner1, value: COST}).should.be.rejectedWith(EVM_REVERT)
+      })
+
+      it('Prevents minting if already owned', async () => {
+        await land.mint(1, {from: owner1, value: COST})
+        await land.mint(1, {from: owner2, value: COST}).should.be.rejectedWith(EVM_REVERT)
+      })
+
+    })
   })
 });
