7d9f1d85d060ad5a174eb17029fa97519d3b16d1
Elizabeth Eidelson <fluturecode@gmail.com>
2022-03-28 22:01:18 -0400

    added minting unit test to Land.test.js

--- test/Land.test.js
+++ test/Land.test.js
@@ -2,7 +2,7 @@ const Land = artifacts.require('./Land');
 
 require('chai').use(require('chai-as-promised')).should();
 
-contract('Land', () => {
+contract('Land', ([owner1, owner2]) => {
   const NAME = 'Fluture Buildings';
   const SYMBOOL = 'DUB';
   const COST = web3.utils.toWei('1', 'ether');
@@ -20,23 +20,41 @@ contract('Land', () => {
     });
 
     it('Returns the symbol', async () => {
-      result = await land.symbol()
-      result.should.equal(SYMBOL)
-  })
+      result = await land.symbol();
+      result.should.equal(SYMBOL);
+    });
 
-  it('Returns the cost to mint', async () => {
-      result = await land.cost()
-      result.toString().should.equal(COST)
-  })
+    it('Returns the cost to mint', async () => {
+      result = await land.cost();
+      result.toString().should.equal(COST);
+    });
 
-  it('Returns the max supply', async () => {
-      result = await land.maxSupply()
-      result.toString().should.equal('5')
-  })
+    it('Returns the max supply', async () => {
+      result = await land.maxSupply();
+      result.toString().should.equal('5');
+    });
 
-  it('Returns the number of buildings/land available', async () => {
-      result = await land.getBuildings()
-      result.length.should.equal(5)
-  })
+    it('Returns the number of buildings/land available', async () => {
+      result = await land.getBuildings();
+      result.length.should.equal(5);
+    });
   });
+
+  describe('Minting', () => {
+    describe('Success', () => {
+      result = await land.mint(1, {from: owner1, value: COST})
+       
+      it('Updates the owner address', async () => {
+        result = await land.ownerOf(1)
+        result.should.equal(owner)
+      })
+  
+      it('Updates building details', async () => {
+        result = await land.getBuilding(1)
+        result.owner1.should.equal(owner1)
+      })
+    })
+
+  
+  })
 });
