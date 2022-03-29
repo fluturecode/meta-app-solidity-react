9a9b0063eab0f249d2669917484548142a16e35d
Elizabeth Eidelson <fluturecode@gmail.com>
2022-03-28 22:19:17 -0400

    added Transfers unit tests for success and failure to test/Land.test.js

--- test/Land.test.js
+++ test/Land.test.js
@@ -2,7 +2,7 @@ const Land = artifacts.require('./Land');
 
 require('chai').use(require('chai-as-promised')).should();
 
-const EVM_REVERT = 'VM Exception while processing transaction: revert'
+const EVM_REVERT = 'VM Exception while processing transaction: revert';
 
 contract('Land', ([owner1, owner2]) => {
   const NAME = 'Fluture Buildings';
@@ -44,33 +44,75 @@ contract('Land', ([owner1, owner2]) => {
 
   describe('Minting', () => {
     describe('Success', () => {
-      result = await land.mint(1, {from: owner1, value: COST})
-       
+      beforeEach(async () => {
+        result = await land.mint(1, { from: owner1, value: COST });
+      });
+
       it('Updates the owner address', async () => {
-        result = await land.ownerOf(1)
-        result.should.equal(owner)
-      })
-  
+        result = await land.ownerOf(1);
+        result.should.equal(owner);
+      });
+
       it('Updates building details', async () => {
-        result = await land.getBuilding(1)
-        result.owner1.should.equal(owner1)
-      })
-    })
+        result = await land.getBuilding(1);
+        result.owner.should.equal(owner1);
+      });
+    });
 
     describe('Failure', () => {
       it('Prevents mint with 0 value', async () => {
-        await land.mint(1, {from: owner1, value: 0}).should.be.rejectedWith(EVM_REVERT)
-      })
+        await land
+          .mint(1, { from: owner1, value: 0 })
+          .should.be.rejectedWith(EVM_REVERT);
+      });
 
       it('Prevents mint with invalid ID', async () => {
-        await land.mint(100, {from: owner1, value: COST}).should.be.rejectedWith(EVM_REVERT)
-      })
+        await land
+          .mint(100, { from: owner1, value: COST })
+          .should.be.rejectedWith(EVM_REVERT);
+      });
 
       it('Prevents minting if already owned', async () => {
-        await land.mint(1, {from: owner1, value: COST})
-        await land.mint(1, {from: owner2, value: COST}).should.be.rejectedWith(EVM_REVERT)
-      })
+        await land.mint(1, { from: owner1, value: COST });
+        await land
+          .mint(1, { from: owner2, value: COST })
+          .should.be.rejectedWith(EVM_REVERT);
+      });
+    });
 
-    })
-  })
+    describe('Transfers', () => {
+      describe('succes', () => {
+        beforeEach(async () => {
+          await land.mint(1, { from: owner1, value: COST });
+          await land.approve(owner2, 1, { from: owner1 });
+          await land.transferFrom(owner1, owner2, 1, { From: owner2 });
+        });
+
+        it('Updates the owner address', async () => {
+          result = await land.ownerOf(1);
+          result.should.equal(owner2);
+        });
+
+        it('Updates building details', async () => {
+          result = await land.getBuilding(1);
+          result.owner.should.equal(owner2);
+        });
+      });
+
+      describe('failure', () => {
+        it('Prevents transfers without ownership', async () => {
+          await land
+            .transferFrom(owner1, owner2, 1, { from: owner2 })
+            .should.be.rejectedWith(EVM_REVERT);
+        });
+
+        it('Prevents transfers without approval', async () => {
+          await land.mint(1, { from: owner1, value: COST });
+          await land
+            .transferFrom(owner1, owner2, 1, { from: owner2 })
+            .should.be.rejectedWith(EVM_REVERT);
+        });
+      });
+    });
+  });
 });
