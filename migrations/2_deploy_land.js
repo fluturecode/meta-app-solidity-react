9d0b73d2b142c0f8f25c123a2e6a456879b1eb70
Elizabeth Eidelson <fluturecode@gmail.com>
2022-03-30 18:56:26 -0400

    fixed typo in 2_deploy_land.js

--- migrations/2_deploy_land.js
+++ migrations/2_deploy_land.js
@@ -3,7 +3,7 @@ const Land = artifacts.require('Land');
 module.exports = async function (deployer) {
   
   const NAME = 'Fluture Buildings'
-  const SYMBOOL = 'DUB'
+  const SYMBOL = 'DUB'
   const COST = web3.utils.toWei('1', 'ether')
 
   await deployer.deploy(Land, NAME, SYMBOL, COST);
